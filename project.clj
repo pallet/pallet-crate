f7d62f6f2732c336bba94dba7240516c88fa576a
Hugo Duncan <hugo@hugoduncan.org>
2013-03-10 11:26:27 -0400

    Updated version for next release cycle

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject pallet-crate/lein-template "0.1.0"
+(defproject pallet-crate/lein-template "0.1.1-SNAPSHOT"
   :description "A leiningen template for pallet crates"
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
